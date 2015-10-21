8089850eabbc090fa4b8244da13c1c21bb559be3
Nick Winters <nwinters3000@gmail.com>
2015-10-21 10:04:39 -0700

    Update license information on code, credits to CC-BY-SA 4.0

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
-# 'Asteroid Impact'
-# Copyright (c) 2015 Nick Winters
-
+# Asteroid Impact (c) by Nick Winters
+# 
+# Asteroid Impact is licensed under a
+# Creative Commons Attribution-ShareAlike 4.0 International License.
+# 
+# You should have received a copy of the license along with this
+# work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>. 
 """
 Game loop and screen management for Asteroid Impact game.
 """
--- virtualdisplay.py
+++ virtualdisplay.py
@@ -1,3 +1,10 @@
+# Asteroid Impact (c) by Nick Winters
+# 
+# Asteroid Impact is licensed under a
+# Creative Commons Attribution-ShareAlike 4.0 International License.
+# 
+# You should have received a copy of the license along with this
+# work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>. 
 """
 Virtual display transformation back and forth from screen coordinates and rectangles
 to game coordinates and rectangles.
