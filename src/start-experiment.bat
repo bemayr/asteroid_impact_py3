9a3c7b7ed6fe59c01cd45a3f70110fd8310413f1
bemayr <bernhard@mayr.io>
2022-04-05 16:45:34 +0200

    Fixed Typo in Start-Script

--- src/start-experiment.bat
+++ src/start-experiment.bat
@@ -1,3 +1,3 @@
 @echo off
-set /p id="Propand:innen ID: "
+set /p id="Proband:innen ID: "
 python .\game.py --script-json .\experiment.json --display-mode fullscreen --subject-number %id% --log-filename .\logs\%id%.csv --log-overwrite false
